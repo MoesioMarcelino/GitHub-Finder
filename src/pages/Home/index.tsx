1fb60cbec7cb0d0374c1169a42d969a7a96eb586
MoesioMarcelino <moesiomarcelino1@gmail.com>
2021-02-23 23:00:26 -0300

    Base da aplicação

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,129 @@
+import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronRight } from 'react-icons/fi';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+import {
+  HeaderContainer,
+  Title,
+  Logo,
+  Form,
+  Repositories,
+  Error,
+} from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Home: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories'
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories)
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    e: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    e.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca pelo repositório');
+    }
+  }
+
+  return (
+    <>
+      <HeaderContainer>
+        <Title>Greenmile Finder</Title>
+        <Logo src={logoImg} alt="Logo" />
+      </HeaderContainer>
+
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Type user nickname here"
+        />
+        <button type="submit">Search</button>
+      </Form>
+
+      {inputError && <Error>{inputError}</Error>}
+
+      <Repositories>
+        {repositories.map((repository) => (
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
+
+      <MapContainer
+        center={[-7.2303004, -39.310245]}
+        zoom={15}
+        style={{ height: '500px' }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,110 @@
+import styled, { css } from 'styled-components';
+
+interface FormProps {
+  hasError: boolean;
+}
+
+export const HeaderContainer = styled.div`
+  display: flex;
+`;
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  margin-top: 80px;
+  max-width: 450px;
+  line-height: 56px;
+`;
+
+export const Logo = styled.img`
+  width: 70px;
+`;
+
+export const Form = styled.form<FormProps>`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    font-size: 16px;
+    border: 2px solid #fff;
+    border-right: 0;
+    ${(props) =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+  button {
+    width: 210px;
+    height: 70px;
+    border: 0;
+    border-radius: 0 5px 5px 0;
+    background: #04d361;
+    color: #fff;
+    font-weight: bold;
+    transition: background 0.4s;
+
+    &:hover {
+      background: #05a54d;
+    }
+  }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.4s;
+    & + a {
+      margin-top: 16px;
+    }
+    &:hover {
+      transform: translateX(10px);
+    }
+    img {
+      width: 70px;
+      height: 70px;
+      border-radius: 50%;
+    }
+    div {
+      margin-left: 16px;
+      flex: 1;
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
